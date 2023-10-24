49fd7bc0c8ce4218c95cf63d355229e8616ed716
Ricardo Moura <ricardodsm27+github@gmail.com>
2023-10-24 17:24:51 +0100

    Tomorrow Forecast DONE

--- WeatherAPI/WeatherAPI.py
+++ WeatherAPI/WeatherAPI.py
@@ -48,22 +48,14 @@ def get_current_weather():
 	print(f"The current feelslike temperature is {feelslike_c}ºC")
 	print(f"The current condition is '{condition}'")
 	print(f"The current wind is {wind_kph}km/h")
-	print(f"The current gust of wind is {wind_kph}km/h")
-	print(f"The current wind direction is {wind_kph}")
+	print(f"The current gust of wind is {gust_kph}km/h")
+	print(f"The current wind direction is {wind_dir}º")
 	print(f"The current precipitation is {precip_mm} mm")
 	print(f"The current humidity is {humidity}%")
 	print(f"The current visibility is {vis_km}km\n")
 
 def get_forecast_day1():
-	forescast = str
-	#forecast1 = res["forecast"]["forecastday"][1]["date"]
-
-	#FORECAST_Prints
-	#print(f"The forecast for tomorrow is:{forecast1}")
-
-while True:
-	### API ###
-	city = str
+		## API ##  ## API ##  ## API ##
 	url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 	querystring = {"q":{city},"days":"3"}
 	headers = {
@@ -72,12 +64,48 @@ while True:
 				}
 	response = requests.get(url, headers=headers, params=querystring)
 	res = response.json()
-	
+		## API ##  ## API ##  ## API ##
+
+	###DATA###
+	#location
+	region = res["location"]["name"]
+	country = res["location"]["country"]
+	#forecast day 1
+	last_updated = res["current"]["last_updated"]
+	forecast_date = res["forecast"]["forecastday"][1]["date"]
+	min_temp_c = res["forecast"]["forecastday"][1]["day"]["mintemp_c"]
+	max_temp_c = res["forecast"]["forecastday"][1]["day"]["maxtemp_c"]
+	avg_temp_c = res["forecast"]["forecastday"][1]["day"]["avgtemp_c"]
+	max_wind_kph = res["forecast"]["forecastday"][1]["day"]["maxwind_kph"]
+	totalprecip_mm = res["forecast"]["forecastday"][1]["day"]["totalprecip_mm"]
+	avgvis_km = res["forecast"]["forecastday"][1]["day"]["avgvis_km"]
+	avg_humidity = res["forecast"]["forecastday"][1]["day"]["avghumidity"]
+	chance_of_rain = res["forecast"]["forecastday"][1]["day"]["daily_chance_of_rain"]
+	chance_of_snow = res["forecast"]["forecastday"][1]["day"]["daily_chance_of_snow"]
+	condition = res["forecast"]["forecastday"][1]["day"]["condition"]["text"]
+
+	#PRINTS
+
+	print(f"\nThe following forecast is from {region}, {country}")
+	print(f"The following data was last update at {last_updated}")
+	print(f"The following data is for tomorrow,{forecast_date}\n")
+	print(f"The average temperature for tomorrow {avg_temp_c}ºC")
+	print(f"The minimum temperature for tomorrow is {min_temp_c}ºC")
+	print(f"The max temperature for tomorrow is {max_temp_c}ºC")
+	print(f"The condition for tomorrow is '{condition}'")
+	print(f"The max wind speed for tomorrow is {max_wind_kph}km/h")
+	print(f"The total precipitation for tomorrow is {totalprecip_mm} mm")
+	print(f"The chance of rain for tomorrow is {chance_of_rain}%")
+	print(f"The chance of snow for tomorrow is {chance_of_snow}%")
+	print(f"The average humidity for tomorrow is {avg_humidity}%")
+	print(f"The average visibility for tomorrow is {avgvis_km}km\n")
+
+while True:
 	### MENU ###
 	print("Welcome to the Weather Station!\n")
 	print("1 - Choose a city.")
 	print("2 - Check current weather.")
-	print("3 - Check forecast for tomorrow.(NOT DONE)")
+	print("3 - Check forecast for tomorrow.")
 	print("4 - Check forecast for after tomorrow.(NOT DONE)")
 	print("5 - Exit.")
 	question = input("Choose an option -> ")
@@ -87,7 +115,11 @@ while True:
 			city = input("Whats the city?\n")
 			print(f"{city} was defined as the city.\n")
 		case "2":
+			print("__________________________________________________________")
 			get_current_weather()
+			print("__________________________________________________________")
+		case "3":
+			get_forecast_day1()
 		case "5":
 			break
 		case _:
