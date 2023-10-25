1d38364df4dc0587612c6f4313957f0bfcfd894d
Ricardo Moura <ricardodsm27+github@gmail.com>
2023-10-25 22:32:35 +0200

    New Project

--- Leap_Year_Finder.py
+++ Leap_Year_Finder.py
@@ -1,3 +1,13 @@
+"""
+Desenvolva um algoritmo que:
+Leia um número representando um determinado ano. Descobrir se esse número (ano)
+representa um ano bissexto. Um ano é bissexto se cumprir a seguintes condições:
+(1) se for divisível por 4,
+(2) se não for divisível por 100.
+Há uma excepção a esta última regra:
+(3) no caso de ser divisível por 100, será bissexto se for divisível por 400.
+"""
+
 year = int(input("Choose a year: "))
 if year % 4 == 0 and year % 100 != 00 or year % 400 == 0:
     print("Nice, it's a leap year.")
