e7533e759924b33676462aedf39fbc20868dce8a
Ricardo Moura <ricardodsm27+github@gmail.com>
2023-11-29 15:12:07 +0000

    Updates

--- ISLA/1 Ano/Fundamentos Progamação/Projeto Final/main.py
+++ ISLA/1 Ano/Fundamentos Progamação/Projeto Final/main.py
@@ -1,12 +1,18 @@
+#Delays
 import time
+#Para formatar melhor o terminal
 from rich.console import Console
 from rich.table import Table
 from rich.layout import Layout
 from rich.panel import Panel
 from rich import box
-
+#Para export e import
+import json
+import os
 #Para usar console.print()
 console = Console()
+#Para usar variavel na Função ls_dir e no Menu
+ultimo_index = -1
 
 filmes_testes = {
     "Matrix":             18,
@@ -17,7 +23,7 @@ filmes_testes = {
     "Interestelar":       20,
     "O Senhor dos Anéis": 14,
     "Forrest Gump":       16,
-    "A ":           19, 
+    "A":                  19, 
     "Cidade de Deus":     13
 }
 #Dicionario para guardar {Filmes: Pontuação}
@@ -45,13 +51,13 @@ def introduzir_dados():
         filmes[filme_input] = pontuacao_input
 def gerar_dados():
     global filmes
-    print("A gerar dados.")
-    #time.sleep(0.6)
-    print("A gerar dados..")
-    #time.sleep(0.6)
-    print("A gerar dados...")
-    #time.sleep(0.5)
-    console.print("[b]Dados gerados.[/b]\n")
+    console.print("A gerar dados.", style='yellow1')
+    time.sleep(0.6)
+    console.print("A gerar dados..", style='yellow1')
+    time.sleep(0.6)
+    console.print("A gerar dados...", style='yellow1')
+    time.sleep(0.6)
+    console.print("[b]Dados gerados[/b]\n", style='yellow1')
     filmes = filmes_testes.copy()
 def alterar_dados():
     separador()
@@ -59,11 +65,11 @@ def alterar_dados():
     console.print("Alterar Dados", style="yellow1", end="")
     console.print(" -------------")
     separador()
-    filme_alterar = input("Que filme quer alterar? ")
+    filme_alterar = console.input("[yellow1]Que filme quer alterar? [/yellow1]")
     if filme_alterar in filmes:
         while True: #Pede pontuação até que seja um valor entre 0 e 20.
             try:    #Necessário usar try para o caso de colocarem TEXTO no input de PONTUAÇÃO, caso metam texto, vai dar o EXCEPT
-                pontuacao_input = int(input("Qual é a nova pontuação? "))                      #e volta a tentar até ter inteiro
+                pontuacao_input = int(console.input("[yellow1]Qual é a nova pontuação? [/yellow1]"))                      #e volta a tentar até ter inteiro
                 if 0 <= pontuacao_input <= 20:
                     break
                 else:
@@ -71,21 +77,25 @@ def alterar_dados():
             except ValueError:
                 print("Por favor, insira um valor inteiro.")
         filmes[filme_alterar] = pontuacao_input
-        console.print(f"O filme [b]{filme_alterar}[/b] foi alterado para [b]{pontuacao_input}[/b] pontos.\n", highlight=False)
+        console.print(f"O filme [yellow1][b]{filme_alterar}[/b][/yellow1] foi alterado para [yellow1][b]{pontuacao_input}[/b][/yellow1] pontos.", highlight=False)
+        separador()
     else:
-        console.print(f"O filme [b]{filme_alterar}[/b] não foi encontrado na lista.\n")
+        console.print(f"O filme [yellow1][b]{filme_alterar}[/b][/yellow1] não foi encontrado na lista.")
+        separador()
 def eliminar_dados():
     separador()
     console.print("------------ ", end="")
     console.print("Eliminar Dados", style="yellow1", end="")
     console.print(" ------------")
     separador()
-    filme_eliminar = input("Que filme quer eliminar? ")
+    filme_eliminar = console.input("[yellow1]Que filme quer eliminar? [/yellow1]")
     if filme_eliminar in filmes:
         del filmes[filme_eliminar]
-        console.print(f"O filme [b]{filme_eliminar}[/b] foi eliminado.\n")
+        console.print(f"O filme [yellow1][b]{filme_eliminar}[/b][/yellow1] foi eliminado.")
+        separador()
     else:
-        console.print(f"O filme [b]{filme_eliminar}[/b] não foi encontrado na lista.\n")
+        console.print(f"O filme [yellow1][b]{filme_eliminar}[/b][/yellow1] não foi encontrado na lista.")
+        separador()
 def consultar_dados():
     #Criar tabela para dados.
     table = Table(title="[b]Filmes - Óscares[/b]",title_style="yellow1", box=box.ROUNDED, border_style="medium_spring_green")
@@ -108,13 +118,15 @@ def pesquisar_dados():
     console.print(" Pesquisar ", style="yellow1", end="")
     console.print(" --------------")
     separador()
-    pesquisar_input = input("Qual é o filme? ")
+    pesquisar_input = console.input("[yellow1]Qual é o filme? [/yellow1]")
     for filme, pontuacao in filmes.items():
         if pesquisar_input == filme:
-            console.print(f"O filme [b]{pesquisar_input}[/b] tem pontuação de [b]{pontuacao}[/b].\n", highlight=False)
+            console.print(f"O filme [yellow1][b]{pesquisar_input}[/b][/yellow1] tem pontuação de [yellow1][b]{pontuacao}[/b][/yellow1].", highlight=False)
+            separador()
             break
     else:
-        console.print(f"O filme [b]{pesquisar_input}[/b] não está na lista.\n")
+        console.print(f"O filme [yellow1][b]{pesquisar_input}[/b][/yellow1] não está na lista.")
+        separador()
 def podio_dados():
     filmes_ordenados = dict(sorted(filmes.items(), key = lambda ordenar_pelo: ordenar_pelo[1], reverse=True))   
     filmes_ordenados_iter = iter(filmes_ordenados)        #Itera pelo dicionario e pega na primeira key usando o next().
@@ -148,7 +160,7 @@ def podio_dados():
     #PRINT PODIO 
     layout = Layout()
     layout.split_row(Podio1, Podio2, Podio3) #Mete tabelas lado a lado
-    console.print(Panel(layout, title="[b]### Pódio ###[/b]",border_style="blue1" ,height=9, width=120), justify="center") #Print com panel à volta
+    console.print(Panel(layout, title="[b]### Pódio ###[/b]",border_style="dodger_blue2" ,height=9, width=120), justify="center") #Print com panel à volta
     
     #Tabela Restantes
     tabela_restantes = Table(title="[b]Restantes Ordenados[/b]", border_style="medium_spring_green",box=box.ROUNDED, title_style="medium_spring_green", title_justify="center")
@@ -168,11 +180,40 @@ def podio_dados():
         
     # Dar print à tabela
     console.print(tabela_restantes, justify="center")
-
 def guardar_dados():
-    pass
+    global filmes
+    separador()
+    console.print("-------------- ", end="")
+    console.print("Exportar", style="yellow1", end="")
+    console.print(" ----------------")
+    separador()
+    ficheiro = console.input("[yellow1]Nome do ficheiro? [/yellow1]")
+    if os.path.exists(f'G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/{ficheiro}.json'):
+        console.print("Não pode guardar um ficheiro com um nome já existente.", style='red1')
+        separador()
+    else:
+        with open(f'G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/{ficheiro}.json', 'w') as fl:
+            json.dump(filmes, fl)
+            console.print("Lista de Filmes foi guardada com éxito!", style="yellow1")
+            separador()
 def carregar_dados():
-    pass
+    global filmes
+
+    separador()
+    console.print("-------------- ", end="")
+    console.print("Importar", style="yellow1", end="")
+    console.print(" ----------------")
+    separador()
+
+    ficheiro = console.input("[yellow1]Nome do ficheiro que quer abrir? [/yellow1]")
+    if os.path.exists(f'G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/{ficheiro}.json'):
+        with open(f'G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/{ficheiro}.json', 'r') as fl:
+            filmes = json.load(fl)
+            console.print("Ficheiro carregado com sucesso.", style='yellow1')
+            separador()
+    else:
+        console.print("Esse ficheiro não existe.", style='red1')
+        separador()
 def sair():
     print("A sair.")
     time.sleep(0.6)
@@ -181,13 +222,67 @@ def sair():
     print("A sair...")
     time.sleep(0.6)
     exit()
+#Extras
+def eliminar_lista():
+    while True:
+        opcao = console.input("[yellow1]Tem a certeza que quer apagar a lista de filmes toda?[/yellow1] (y/n) ")
+        if opcao == "y":
+            filmes.clear()
+            console.print("[yellow1][b]Lista Apagada.[/b][/yellow1]")
+            time.sleep(0.5)
+            break
+        elif opcao == "n":
+            console.print("[yellow1][b]Lista Intacta.[/b][/yellow1]")
+            time.sleep(0.5)
+            break
+        else:
+            console.print("[red1]Opção Invalida.[/red1]")
+def ls_dir():
+    separador()
+    console.print("-------------- ", end="")
+    console.print("Diretório", style="yellow1", end="")
+    console.print(" ---------------")
+    separador()
 
-def extra_filme_info(): #API? lib IMBD?
+    path = "G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/"
+    files = os.listdir(path)
+
+    console.print("[yellow1]Ficheiro_Exportados/[/yellow1]")
+    global ultimo_index
+    ultimo_index = int(len(files) - 1)
+    if ultimo_index < 0:
+        print("└──Pasta Vazia")
+    else:
+        for file in files:
+            if file == files[ultimo_index]:
+                print(f"└──{file}")
+            else:
+                print(f"├──{file}")
+    separador()
+
+def apagar_exportado():
+    separador()
+    console.print("---------- ", end="")
+    console.print("Eliminar Ficheiro", style="yellow1", end="")
+    console.print(" -----------")
+    separador()
+
+    ficheiro = console.input("[yellow1]Nome do ficheiro? [/yellow1]")
+
+    if os.path.exists(f'G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/{ficheiro}.json'):
+            os.remove(f'G:/O meu disco/GitHub/Projects-ITGraduation/ISLA/1 Ano/Fundamentos Progamação/Projeto Final/Ficheiros_Exportados/{ficheiro}.json')
+            console.print(f"[yellow1]{ficheiro}.json[/yellow1] foi removido com éxito!")
+            separador()
+    else:
+            console.print("[red1]Esse ficheiro não existe.[/red1]")
+            separador()
+def info_filme(): #API? lib IMBD?
     pass
+
 
 ### Menu ###
 while True: #Repete sempre para mostrar sempre o menu até escolher opção sair.
-    console.print(" ### [b]Menu[/b] ###", style="blue1")
+    console.print("\n ### [b]Menu[/b] ###", style="yellow1")
     print(" 1. Introdução dados")
     print(" 2. Geração dados (preenchimento dos vetores (lista) de forma automática)")
     print(" 3. Alterar dados")
@@ -198,7 +293,11 @@ while True: #Repete sempre para mostrar sempre o menu até escolher opção sair
     print(" 8. Guardar em ficheiro")
     print(" 9. Carregar dados do ficheiro")
     print("10. Sair")
-    
+    console.print("\n ### [b]Extras[/b] ###", style="yellow1")
+    print("15. Eliminar Lista.")
+    print("16. Ver ficheiros exportados")
+    print("17. Apagar Ficheiros Exportados")
+    print("18. API - Info Filme\n")
     menu = input("Escolha uma opção do menu: ")
     print("")
     match menu:
@@ -208,42 +307,61 @@ while True: #Repete sempre para mostrar sempre o menu até escolher opção sair
             if len(filmes) == 0: # Se Lista estiver vazia, faz a função.
                 gerar_dados()
             else:
-                console.print("Lista de Filmes já não está vazia.\n", style="red1")
+                console.print("Lista de Filmes já não está vazia.", style="red1")
         case "3":
             if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
                 alterar_dados()
             else:
-                console.print("Lista de Filmes ainda está vazia.\n", style="red1")
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
         case "4":
             if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
                 eliminar_dados()
             else:
-                console.print("Lista de Filmes ainda está vazia.\n", style="red1")
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
         case "5":
             if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
                 consultar_dados()
             else:
-                console.print("Lista de Filmes ainda está vazia.\n", style="red1")
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
         case "6":
             if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
                 pesquisar_dados()
             else:
-                console.print("Lista de Filmes ainda está vazia.\n", style="red1")
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
         case "7":
             if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
                 podio_dados()
             else:
-                console.print("Lista de Filmes ainda está vazia.\n", style="red1")
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
         case "8":
             if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
                 guardar_dados()
             else:
-                console.print("Lista de Filmes ainda está vazia.\n", style="red1")
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
         case "9":
-            carregar_dados()
+            if len(filmes) == 0: # Se Lista estiver vazia, faz a função.
+                carregar_dados()
+            else:
+                console.print("Lista de Filmes já não está vazia.", style="red1")
         case "10":
             sair() # Pode-se usar break para sair do loop e teria o mesmo efeito.
+
+            ##### EXTRAS ####
+        case "15":
+            if len(filmes) != 0: # Se Lista estiver vazia, não faz a função.
+                eliminar_lista()
+            else:
+                console.print("Lista de Filmes ainda está vazia.", style="red1")
+        case "16":
+            ls_dir()
+        case "17":
+            if ultimo_index < 0:
+                console.print("Não existe ficheiros na pasta para eliminar.", style="red1")
+            else:
+                apagar_exportado()
+        case "18":
+            info_filme()      
         case _:
-            console.print("Tem de escolher um numero do menu.\n", style="red1")
+            console.print("Tem de escolher um numero do menu.", style="red1")
 
     
