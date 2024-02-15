d37029c89d65ab03e9b1ff7fb182006a60b4f25e
Ricardo Moura <ricardodsm27+github@gmail.com>
2024-02-15 17:55:01 +0000

    updates

--- ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/001 - Array Simples/obj/Debug/net8.0/001 - Array Simples.AssemblyInfo.cs
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/001 - Array Simples/obj/Debug/net8.0/001 - Array Simples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001 - Array Simples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17d321a9a281442c78b4721d7485b21ecf50569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26f79686e4d381f538e6c6197eff500358f8ee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("001 - Array Simples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001 - Array Simples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
