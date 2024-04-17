4cd8887d4f217931da464d965657b9182690b3fe
Ricardo Moura <ricardodsm27+github@gmail.com>
2024-04-17 11:46:14 +0100

    update

---
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/004 - StructComArray/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _004___StructComArray
+{
+    internal class Program
+    {//Feito por: Ricardo Moura a22309442
+        struct Aluno
+        {
+            public string nome;
+            public double[] notas;
+        }
+        static void Main(string[] args)
+        {
+            //Definir Variaveis
+            int numAlunos = 3;
+            int numDisciplinas = 5;
+            Aluno[] aAlunos = new Aluno[numAlunos];
+
+            int opcao = 0;
+            do
+            {
+                Console.WriteLine("------------MENU------------");
+                Console.WriteLine("1 - Inserir Dados.");
+                Console.WriteLine("2 - Listar Alunos.");
+                Console.WriteLine("3 - ");
+                Console.WriteLine("4 - ");
+                Console.WriteLine("5 - ");
+                Console.WriteLine("6 - ");
+                Console.WriteLine("7 - ");
+                Console.WriteLine("8 - ");
+                Console.WriteLine("0 - Sair do programa");
+                Console.WriteLine("----------------------------");
+
+                Console.Write("Digite uma opção.");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1://Preencher struct
+                        for (int i = 0; i < numAlunos; i++)
+                        {
+                            Console.WriteLine($"Escreve o nome do {i + 1} aluno: ");
+                            aAlunos[i].nome = Console.ReadLine();
+
+                            aAlunos[i].notas = new double[numDisciplinas];
+                            for (int j = 0; j < aAlunos[i].notas.Length; j++)
+                            {
+                                Console.WriteLine($"Disciplina: {j + 1} - Nota: ");
+                                aAlunos[i].notas[j] = Convert.ToDouble(Console.ReadLine());
+                            }
+                        }
+                        Console.Clear();
+                        break;
+                    case 2://Listar Alunos
+                        for (int i = 0; i < aAlunos.Length; i++)
+                        {
+                            Console.WriteLine($"Aluno{i + 1}: {aAlunos[i].nome}");
+
+                            for (int j = 0; j < aAlunos[i].notas.Length; j++)
+                            {
+                                Console.WriteLine($"Disciplina {i + 1}: {aAlunos[i].notas[j]}");
+                            }
+                        }
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Opção não existe.");
+                        break;
+                }
+            } while (opcao != 0);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/005 - StructDentroDeStruct/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _005___StructDentroDeStruct
+{
+    internal class Program
+    {
+        struct Aluno
+        {
+            public string nome;
+            public Disciplina[] aDisciplinasAluno;
+        }
+        struct Disciplina
+        {
+            public int codigo;
+            public string designacao;
+            public double nota;
+        }
+        static void Main(string[] args)
+        {
+            int numAluno = 3;
+            int numDiscplinas = 4;
+
+            Aluno[] aAlunos = new Aluno[numAluno];
+            Disciplina[] aDisciplinasExistentes = new Disciplina[numDiscplinas];
+
+            //Preencer as disciplinas existenes, a nota apenas é preenchido com o Aluno
+            for (int i = 0; i < aDisciplinasExistentes.Length; i++)
+            {
+                Console.WriteLine($"Escreve o código da disciplina {i + 1}: ");
+                aDisciplinasExistentes[i].codigo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Designação: ");
+                aDisciplinasExistentes[i].designacao = Console.ReadLine();
+            }
+
+            for (int i = 0; i < aAlunos.Length; i++)
+            {
+                Console.WriteLine($"Nome do aluno {i + 1}");
+                aAlunos[i].nome = Console.ReadLine();
+                aAlunos[i].aDisciplinasAluno = aDisciplinasExistentes;
+
+                for (int k = 0; k < aAlunos[i].aDisciplinasAluno.Length; k++)
+                {
+                    Console.WriteLine($"Disciplina: {aAlunos[i].aDisciplinasAluno[k].codigo} - {aAlunos[i].aDisciplinasAluno[k].designacao}");
+                    Console.WriteLine($"Nota: ");
+                    aAlunos[i].aDisciplinasAluno[k].nota = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            //LISTAR ALUNOS
+
+            for (int i = 0; i < aAlunos.Length; i++)
+            {
+                Console.WriteLine($"Nome do aluno {i + 1}: {aAlunos[i].nome}");
+                for (int j = 0; j < aAlunos[i].aDisciplinasAluno.Length; j++)
+                {
+                    Console.WriteLine($"Disciplina: {aAlunos[i].aDisciplinasAluno[j].codigo} - {aAlunos[i].aDisciplinasAluno[j].designacao}");
+                    Console.WriteLine($"Nota: {aAlunos[i].aDisciplinasAluno[j].nota}");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
