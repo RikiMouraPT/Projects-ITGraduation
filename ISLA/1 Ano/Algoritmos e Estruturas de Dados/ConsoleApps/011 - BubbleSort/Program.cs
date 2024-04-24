c70d204551bbf059b87f0ae61f3422e3389b4b91
Ricardo Moura <ricardodsm27+github@gmail.com>
2024-04-24 22:33:39 +0200

    updates

---
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/011 - BubbleSort/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _011___BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 5, 2, 1 };
+
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 0; j < nums.Length - 1; j++)
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(nums[i]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/012 - Recursividade/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _012___Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o número? ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Resultado do fatorial de {num} é {fatorial(num)}");
+            Console.ReadKey();
+        }
+        public static double fatorial(double n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else 
+            {
+                return n * fatorial(n - 1);
+            }
+        }
+    }
+}
