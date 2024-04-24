6a934a568d12cfbadf3f99fe17ce42f1f05e7034
Ricardo Moura <ricardodsm27+github@gmail.com>
2024-04-24 20:18:16 +0200

    updates

---
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/009 - Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _009___Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] fib10 = new int[10];
+            fib10[0] = 0;
+            fib10[1] = 1;
+
+            Console.Write($"{fib10[0]}, {fib10[1]}, ");
+            for (int i = 2; i < fib10.Length; i++) 
+            {
+                fib10[i] = fib10[i - 1] + fib10[i - 2];
+                Console.Write($"{fib10[i]}, ");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ISLA/1 Ano/Algoritmos e Estruturas de Dados/ConsoleApps/010 - SomasSucessivas/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _010___SomasSucessivas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            double soma = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write($"Qual o {i+1}º número: ");
+                double  numero = Convert.ToDouble(Console.ReadLine());
+                soma += numero;
+            }
+            double media = soma / num;
+            Console.WriteLine($"Média = {media}");
+        }
+    }
+}
