889bb57ec34cfe49cb254e4915f2aac78fa62d42
Ricardo Moura <ricardodsm27+github@gmail.com>
2024-09-20 19:56:16 +0100

    update to english and folders

--- Taekwondo/Python/participantes.py
+++
@@ -1,3 +0,0 @@
-participantes = ["Ricardo", "Vitor", "José", "Martim", "Ana", "Claudia", 
-                 "Rodrigo", "Isabel", "Miguel", "Sofia", "Carlos", "Beatriz", 
-                 "Pedro", "Catarina", "Francisco", "Inês"]
