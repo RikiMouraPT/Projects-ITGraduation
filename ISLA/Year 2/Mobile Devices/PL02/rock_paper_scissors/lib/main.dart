14cab9e3283501402f6ac0ea2b1b3fcff24ab218
RicardoMoura <ricardodsm27@gmail.com>
2025-03-19 21:19:24 +0000

    Implement Rock-Paper-Scissors game logic and UI with asset images

--- ISLA/Year 2/Mobile Devices/PL02/rock_paper_scissors/lib/main.dart
+++ ISLA/Year 2/Mobile Devices/PL02/rock_paper_scissors/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,23 +31,118 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String status = "Choose";
+  String botImage = "assets/papel.jpg";
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void game(String userOption)
+  {
+    int botOption = Random().nextInt(3);
+    String botOptionString = "";
+    
+    switch(botOption){
+      case 0:
+        botOptionString = 'rock';
+        setState(() {
+          botImage = "assets/pedra.jpg";
+        });
+        break;
+      case 1:
+        botOptionString = 'paper';
+        setState(() {
+          botImage = "assets/papel.jpg";
+        });
+        break;
+      case 2:
+        botOptionString = 'scissors';
+        setState(() {
+          botImage = "assets/tesoura.jpg";
+        });
+        break;
+    }
+
+    if (userOption == botOptionString) {
+      setState(() {
+        status = "Draw!";
+      });
+    }
+    else if (userOption == 'rock' && botOptionString == 'scissors' ||
+            userOption == 'paper' && botOptionString == 'rock' ||
+            userOption == 'scissors' && botOptionString == 'paper') {
+      setState(() {
+        status = "You win!";
+      });
+    }
+    else {
+      setState(() {
+        status = "You lose!";
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text("The App Choice:", style: TextStyle(fontSize: 24)),
+            SizedBox(height: 50),
+            Image(
+              image: AssetImage(botImage),
+              height: 200,
+              width: 200,
+            ),
+            SizedBox(height: 25),
+            Text(status, style: TextStyle(fontSize: 22)),
+            SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 50,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      game("rock");
+                    });
+                  },
+                  child: Image(
+                    image: AssetImage("assets/pedra.jpg"),
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      game("paper");
+                    });
+                  },
+                  child: Image(
+                    image: AssetImage("assets/papel.jpg"),
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      game("scissors");
+                    });
+                  },
+                  child: Image(
+                    image: AssetImage("assets/tesoura.jpg"),
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
