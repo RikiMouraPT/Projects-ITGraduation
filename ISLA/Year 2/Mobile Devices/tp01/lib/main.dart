9b5d3b5dc5c2beb455f0281e1776820782a89334
RicardoMoura <ricardodsm27@gmail.com>
2025-04-09 14:34:36 +0100

    Add number base conversion functionality with validation and UI enhancements

--- ISLA/Year 2/Mobile Devices/tp01/lib/main.dart
+++ ISLA/Year 2/Mobile Devices/tp01/lib/main.dart
@@ -29,8 +29,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> fromFormKey = GlobalKey<FormState>();
+  final TextEditingController fromController = TextEditingController();
 
 
+  int fromBase = 10;
+  int toBase = 10;
+  String fromValue = "";
+  String result = "";
+
+  void fromButton(String base) {
+    setState(() {
+      if (base == "Bin") {
+        fromBase = 2;
+      } else if (base == "Oct") {
+        fromBase = 8;
+      } else if (base == "Dec") {
+        fromBase = 10;
+      } else if (base == "Hex") {
+        fromBase = 16;
+      }
+    });
+  }
+
+  void toButton(String base) {
+    setState(() {
+      if (base == "Bin") {
+        toBase = 2;
+      } else if (base == "Oct") {
+        toBase = 8;
+      } else if (base == "Dec") {
+        toBase = 10;
+      } else if (base == "Hex") {
+        toBase = 16;
+      }
+      calculateResult(fromBase, toBase);
+    });
+  }
+
+  void calculateResult(int fromBase, int toBase) {
+    setState(() {
+      if (fromFormKey.currentState!.validate()) {
+        fromValue = fromController.text;
+
+        int decimalValue = int.parse(fromValue, radix: fromBase);
+
+        result = decimalValue.toRadixString(toBase).toUpperCase();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,12 +88,181 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            SizedBox(height: 50,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Convert from:", 
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
+                ),
+                TextButton(
+                  onPressed: () => fromButton("Bin"), 
+                  child: Text(
+                    "Bin",
+                    style: TextStyle(
+                      backgroundColor: fromBase == 2 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  )),
+                TextButton(
+                  onPressed: () => fromButton("Oct"), 
+                  child: Text(
+                    "Oct",
+                    style: TextStyle(
+                      backgroundColor: fromBase == 8 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => fromButton("Dec"), 
+                  child: Text(
+                    "Dec",
+                    style: TextStyle(
+                      backgroundColor: fromBase == 10 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  )
+                ),
+                TextButton(
+                  onPressed: () => fromButton("Hex"), 
+                  child: Text(
+                    "Hex",
+                    style: TextStyle(
+                      backgroundColor: fromBase == 16 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  )
+                ),
+              ],
+            ),
+            Form(
+              key: fromFormKey,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25), 
+                child: TextFormField(
+                  controller: fromController,
+                  decoration: InputDecoration(
+                    label: Text("Number"),
+                    suffix: Text(
+                      fromBase == 2 ? "bin" 
+                      : fromBase == 8 ? "oct" 
+                      : fromBase == 10 ? "dec" 
+                      : "hex"
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Missing Data";
+                    }
+                    var parsedInt = int.tryParse(value, radix: fromBase);
+                    if (parsedInt == null) {
+                      if (fromBase == 2) {
+                        result = "Enter valid binary values.";
+                        return "Enter valid binary values.";
+                      } else if (fromBase == 8){
+                        result = "Enter valid octal values.";
+                        return "Enter valid octal values.";
+                      }
+                      else if (fromBase == 10) {
+                        result = "Enter valid decimal values.";
+                        return "Enter valid decimal values.";
+                      }
+                      else if (fromBase == 16) {
+                        result = "Enter valid hexadecimal values.";
+                        return "Enter valid hexadecimal values.";
+                      }
+                      else {
+                        result = "Enter valid values.";
+                        return "Enter valid values.";
+                      }
+                    }
+                    if (parsedInt < 0) {
+                      return "Enter a value >= 0";
+                    }
+
+                    return null;
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 50,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Convert to:", 
+                  style: TextStyle(fontSize: 20),
+                ),
+                TextButton(
+                  onPressed: () {
+                    toButton("Bin");
+                    calculateResult(fromBase, toBase);
+                  }, 
+                  child: Text(
+                    "Bin",
+                    style: TextStyle(
+                      backgroundColor: toBase == 2 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  )),
+                TextButton(
+                  onPressed: () {
+                    toButton("Oct");
+                    calculateResult(fromBase, toBase);
+                  },
+                  child: Text(
+                    "Oct",
+                    style: TextStyle(
+                      backgroundColor: toBase == 8 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    toButton("Dec");
+                    calculateResult(fromBase, toBase);
+                  }, 
+                  child: Text(
+                    "Dec",
+                    style: TextStyle(
+                      backgroundColor: toBase == 10 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  )
+                ),
+                TextButton(
+                  onPressed: () {
+                    toButton("Hex");
+                    calculateResult(fromBase, toBase);
+                  },
+                  child: Text(
+                    "Hex",
+                    style: TextStyle(
+                      backgroundColor: toBase == 16 ? Colors.yellowAccent : Colors.transparent,
+                      fontSize: 20
+                    ),
+                  )
+                ),
+              ],
+            ),
+            SizedBox(height: 25),
+            Text(
+              "Result\n $result",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24
+              ),
+            ),
           ],
         ),
       ),
-  
     );
   }
 }
